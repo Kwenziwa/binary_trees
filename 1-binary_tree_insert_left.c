76b0cb7e68201ec67f7af9877ff7ee07d59cdc75
Kwenziwa Lizwi <Kwenziwa@live.com>
2023-08-02 20:58:37 -0400

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,31 +1,31 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_left - Insert a node to the left of its a_parent. If the a_parent 
+ * binary_tree_insert_left - Insert a node to the left of its parent. If the parent 
  * already has a left child, the new node will be placed one level below 
  * the existing left child, becoming the new left child.
- * @a_parent: Rephrase the a parent node of the specified node.
- * @a_vlu: value of the node
+ * @parent: Rephrase the a parent node of the specified node.
+ * @value: value of the node
  * Return: NULL if it fails or the new node
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *a_parent, int a_vlu)
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *adv_node;
 
-	if (a_parent == NULL)
+	if (parent == NULL)
 	{
 		return (NULL);
 	}
 
-	adv_node = binary_tree_node(a_parent, a_vlu);
+	adv_node = binary_tree_node(parent, value);
 	if (adv_node == NULL)
 	{
 		return (NULL);
 	}
-	if (a_parent->left != NULL)
+	if (parent->left != NULL)
 	{
-		adv_node->left = a_parent->left;
-		a_parent->left->a_parent = adv_node;
+		adv_node->left = parent->left;
+		parent->left->parent = adv_node;
 	}
-	a_parent->left = adv_node;
+	parent->left = adv_node;
 	return (adv_node);
 }
