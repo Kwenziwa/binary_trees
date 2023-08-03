ecac8c9e4f536c0f68c99fe84b99d2af52a2734e
Kwenziwa Lizwi <Kwenziwa@live.com>
2023-08-02 21:00:33 -0400

    bug 2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,32 +1,32 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_right - Insert a node to the right of its a_prnt. If the a_prnt 
+ * binary_tree_insert_right - Insert a node to the right of its parent. If the parent 
  * already has a right child, the new node will be placed one level below the 
  * existing right child, becoming the new right child.
- * @a_prnt: parent of the specified node
- * @a_vlu: value of the node
+ * @parent: parent of the specified node
+ * @value: value of the node
  * Return: NULL if it fails or the new node
  */
 
-binary_tree_t *binary_tree_insert_right(binary_tree_t *a_prnt, int a_vlu)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *adv_node;
 
-	if (a_prnt == NULL)
+	if (parent == NULL)
 	{
 		return (NULL);
 	}
 
-	adv_node = binary_tree_node(a_prnt, a_vlu);
+	adv_node = binary_tree_node(parent, value);
 	if (adv_node == NULL)
 	{
 		return (NULL);
 	}
-	if (a_prnt->right != NULL)
+	if (parent->right != NULL)
 	{
-		adv_node->right = a_prnt->right;
-		a_prnt->right->a_prnt = adv_node;
+		adv_node->right = parent->right;
+		parent->right->parent = adv_node;
 	}
-	a_prnt->right = adv_node;
+	parent->right = adv_node;
 	return (adv_node);
 }
